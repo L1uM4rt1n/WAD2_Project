c7dddf2d2c14f17a251428e7d8266454b81fba63
L1uM4rt1n <96768015+L1uM4rt1n@users.noreply.github.com>
2023-09-14 13:45:34 +0800

    satellite map up

---
+++ Backend/Location_MS/location/src/main/java/com/example/location/Maps.java
@@ -0,0 +1,14 @@
+package com.example.location;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@org.springframework.web.bind.annotation.RestController
+
+/* 
+creating a map class such that when the frontend calls the LocationApplication, it will render an entire google map shown
+can use google cloud console API key: AIzaSyA8gdI2sHtDHuo1ZX8Jur8G7dHyN_mj5wQ to show the map
+*/
+@RestController
+public class Maps {
+      
+}
